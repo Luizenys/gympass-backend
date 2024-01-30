fdf7d395e67ac0bd7e5d21e93ecd82e89e1d179a
Luiza Rodrigues <luizars@id.uff.br>
2024-01-29 21:29:49 -0300

    password hash

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma"
 import { z } from "zod"
+import { hash } from 'bcryptjs'
 import { FastifyRequest, FastifyReply } from "fastify"
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -11,11 +12,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     const { name, email, password } = registerBodySchema.parse(request.body)
 
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email,
+        }
+    })
+
+    if (userWithSameEmail) {
+        return reply.status(409).send()
+    }
+
     await prisma.user.create({
         data: {
             name,
             email,
-            password_hash: password
+            password_hash
         }
     })
 
