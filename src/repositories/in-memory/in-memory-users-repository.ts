35b3ae08382802981e29bdd90508e493234f22fa
Luiza Rodrigues <luizars@id.uff.br>
2024-02-01 13:53:36 -0300

    Get user profile use case

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -27,4 +27,14 @@ export class InMemoryUsersRepository implements UsersRepository {
 
         return user
     }
+
+    async findById(id: string) {
+        const user = this.items.find((item) => item.id === id)
+
+        if (!user) {
+            return null
+        }
+
+        return user
+    }
 }
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -20,4 +20,14 @@ export class PrismaUsersRepository implements UsersRepository {
 
         return user
     }
+
+    async findById(id: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                id,
+            }
+        })
+
+        return user
+    }
 }
