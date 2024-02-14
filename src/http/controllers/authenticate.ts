cf51645005fc2027d20e25e258d3c1f981d59497
Luiza Rodrigues <luizars@id.uff.br>
2024-02-14 18:41:17 -0300

    jwt authentication

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -14,10 +14,18 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
     try {
         const authenticateUseCase = makeAuthenticateUseCase()
 
-        await authenticateUseCase.execute({
+        const { user } = await authenticateUseCase.execute({
             email,
             password
         })
+
+        const token = await reply.jwtSign({}, {
+            sign: {
+                sub: user.id
+            }
+        })
+
+        return reply.status(200).send({ token })
     } catch (err) {
         if (err instanceof InvalidCredentialsError) {
             return reply.status(400).send({ message: err.message })
@@ -25,6 +33,4 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 
         throw err
     }
-
-    return reply.status(200).send()
 }
