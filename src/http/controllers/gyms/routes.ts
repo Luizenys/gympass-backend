5418aa3a8edc527ac693d17742b84bc1332ccac0
Luiza Rodrigues <luizars@id.uff.br>
2024-02-15 10:57:10 -0300

    implementing search gym controller and fetch nearby gym controller

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,8 +1,15 @@
 import { FastifyInstance } from "fastify";
 
 import { verifyJWT } from "../../middlewares/verify-jwt";
+import { search } from "./search";
+import { create } from "./create";
+import { nearby } from "./nearby";
 
 export async function gymsRoutes(app: FastifyInstance) {
     app.addHook('onRequest', verifyJWT)
 
+    app.get('/gyms/search', search)
+    app.get('/gyms/nearby', nearby)
+
+    app.post('/gyms', create)
 }
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,25 @@
+import { z } from "zod"
+import { FastifyRequest, FastifyReply } from "fastify"
+import { makeSearchGymsUseCase } from "@/use-cases/factories/make-search-gyms-use-case"
+
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+    const searchGymsQuerySchema = z.object({
+        query: z.string(),
+        page: z.coerce.number().min(1).default(1)
+    })
+
+    const { query, page } = searchGymsQuerySchema.parse(request.body)
+
+ 
+    const searchGymUseCase = makeSearchGymsUseCase()
+
+    const { gyms } = await searchGymUseCase.execute({
+        query,
+        page
+    })
+
+    return reply.status(200).send({
+        gyms
+    })
+}
