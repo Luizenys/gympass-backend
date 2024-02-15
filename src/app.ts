8252f4442ad457d02d2452a2d2d2a6a65c4461b4
Luiza Rodrigues <luizars@id.uff.br>
2024-02-15 20:09:24 -0300

    implementing refresh token

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import { usersRoutes } from "./http/controllers/users/routes";
 import { gymsRoutes } from "./http/controllers/gyms/routes";
 import { checkInsRoutes } from "./http/controllers/check-ins/routes";
@@ -10,8 +11,17 @@ import { checkInsRoutes } from "./http/controllers/check-ins/routes";
 export const app = fastify()
 
 app.register(fastifyJwt, {
-    secret: env.JWT_SECRET
+    secret: env.JWT_SECRET,
+    cookie: {
+        cookieName: 'refreshToken',
+        signed: false
+    },
+    sign: {
+        expiresIn: '10m'
+    }
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
