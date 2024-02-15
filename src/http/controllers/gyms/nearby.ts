06cbab7bc64f243078e84b6ec5df370b6d8bee59
Luiza Rodrigues <luizars@id.uff.br>
2024-02-15 11:54:13 -0300

    implementing check-ins controllers

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -13,7 +13,7 @@ export async function nearby(request: FastifyRequest, reply: FastifyReply) {
         }),
     })
 
-    const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+    const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.query)
 
  
     const fetchNearbyGymUseCase = makeFetchNearbyGymsUseCase()
