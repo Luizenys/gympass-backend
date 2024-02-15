d37358d8c8aefb427f2b09c4808eb4aed500b290
Luiza Rodrigues <luizars@id.uff.br>
2024-02-15 10:38:10 -0300

    implementing create gym controller

---
+++ src/http/controllers/gyms/routes.ts
@@ -0,0 +1,8 @@
+import { FastifyInstance } from "fastify";
+
+import { verifyJWT } from "../../middlewares/verify-jwt";
+
+export async function gymsRoutes(app: FastifyInstance) {
+    app.addHook('onRequest', verifyJWT)
+
+}
